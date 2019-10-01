f271095e3835b2a57c8e17ba5e9efe305caae2a1
Rupal Totale <rupaltotale>
2019-10-01 00:12:53 -0700

    reformatted app class and created home skeleton

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+//import liraries
+import React, { Component } from 'react';
+
+// create a component
+class Home extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="container">
+				<h2>Home</h2>
+			</div>
+		);
+	}
+}
+
+//make this component available to the app
+export default Home;
