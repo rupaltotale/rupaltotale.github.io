bfcb66018a12c259da0d6e5e38448441c77a9bef
Rupal Totale <rupaltotale>
2019-10-03 23:13:38 -0700

    Updates

---
+++ precache-manifest.2bcd62653a79b79def3ed2218d79265b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b86a6ee0655aa2d49a53615cbe738a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2ef41555ab246cef9b",
+    "url": "/static/css/main.bdcedcdf.chunk.css"
+  },
+  {
+    "revision": "3ef3989c1e4ffc6213ad",
+    "url": "/static/js/2.e2c9cc0e.chunk.js"
+  },
+  {
+    "revision": "4b2ef41555ab246cef9b",
+    "url": "/static/js/main.704f22f8.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "eefe5cb5b5cf672d1b1ad576cf38830e",
+    "url": "/static/media/picture.eefe5cb5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a2308bdff65825773526f255f67b4fc.js"
+  "/precache-manifest.2bcd62653a79b79def3ed2218d79265b.js"
 );
 
 self.addEventListener('message', (event) => {
