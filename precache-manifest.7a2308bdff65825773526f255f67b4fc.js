d41ce30b8a2ab32da8f457ae82798c5c681ed895
Rupal Totale <rupaltotale>
2019-10-03 20:37:47 -0700

    Updates

---
+++ precache-manifest.7a2308bdff65825773526f255f67b4fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff63d46f98db2eaf1e76e5271e3f4dc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc66626454624da61e3",
+    "url": "/static/css/main.11d1d8dd.chunk.css"
+  },
+  {
+    "revision": "253d04f37cb91b6b8b6a",
+    "url": "/static/js/2.b569fa88.chunk.js"
+  },
+  {
+    "revision": "ffc66626454624da61e3",
+    "url": "/static/js/main.de547783.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "eefe5cb5b5cf672d1b1ad576cf38830e",
+    "url": "/static/media/picture.eefe5cb5.png"
+  },
+  {
+    "revision": "fbd3ef16e0f717d0ea2ee40f18799d0b",
+    "url": "/static/media/project.fbd3ef16.png"
+  }
+]);
