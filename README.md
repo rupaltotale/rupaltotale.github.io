3457022e1eb968b123905a61978a573661570660
Rupal Totale <rupaltotale>
2019-10-05 23:44:09 -0700

    added TOC to readme and generalized readme

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 # My Personal Website
 
+## Table of Contents
+
 - [My Personal Website](#my-personal-website)
+  - [Table of Contents](#table-of-contents)
   - [How To](#how-to)
     - [Create a web app with React](#create-a-web-app-with-react)
-      - [References](#references)
     - [Use Google Sheets as CMS with tabletop.js](#use-google-sheets-as-cms-with-tabletopjs)
-      - [References](#references-1)
     - [Submit forms with form-data](#submit-forms-with-form-data)
-      - [References](#references-2)
     - [Host/deploy a react web app with GH Pages](#hostdeploy-a-react-web-app-with-gh-pages)
-      - [References](#references-3)
-    - [Use react-rouer for a more snappy web app](#use-react-rouer-for-a-more-snappy-web-app)
-      - [References](#references-4)
+    - [Use react-router for a more snappy web app](#use-react-router-for-a-more-snappy-web-app)
   - [Workplace settings](#workplace-settings)
     - [Editor: VS Code](#editor-vs-code)
+  - [References](#references)
 
-I made this "static" website with the popular web framework React. The web app is hosted on GitHub Pages and is thus static (at least the final product). I discuss how I implemented various features for this website below. 
+I made this "static" website with the popular web framework React. The web app is hosted on GitHub Pages and is thus static (at least the final product). I discuss how I implemented various features for this website below.
 
 ## How To
 
@@ -26,22 +25,12 @@ Some kind of intro
 1. Step 1
 2. Step 2
 
-#### References
-
-- [Example 1](http://example.com/ "Title")
-- [Example 2](http://example.com/ "Title")
-
 ### Use Google Sheets as CMS with tabletop.js
 
 Some kind of intro
 
 1. Step 1
 2. Step 2
-
-#### References
-
-- [Example 1](http://example.com/ "Title")
-- [Example 2](http://example.com/ "Title")
 
 ### Submit forms with form-data
 
@@ -50,12 +39,6 @@ Some kind of intro
 1. Step 1
 2. Step 2
 
-#### References
-
-- [Get Started with form data](https://www.form-data.com/?form=86cf0c10-a4a4-4dff-8bf2-c6518c1d9cfe)
-- [Form resources for static websites](https://gridsome.org/docs/guide-forms/)
-- [Submit a form with nodemailer and express](https://www.youtube.com/watch?v=EPnBO8HgyRU)
-
 ### Host/deploy a react web app with GH Pages
 
 Some kind of intro
@@ -63,31 +46,30 @@ Some kind of intro
 1. Step 1
 2. Step 2
 
-#### References
-
-- [Example 1](http://example.com/ "Title")
-- [Example 2](http://example.com/ "Title")
-
-### Use react-rouer for a more snappy web app
+### Use react-router for a more snappy web app
 
 Use hash router for static pages
 
 1. Step 1
 2. Step 2
 
-#### References
-
-- [Get started with react router](https://reacttraining.com/react-router/web/example/basic)
-- [Install react-router-dom with npm](https://www.npmjs.com/package/react-router-dom)
-
 ## Workplace settings
 
 ### Editor: VS Code
 
-I use the following extensions for react and react-native apps:
+I use the following extensions when working with Javascript:
 
-- Prettier Now
-- Auto Import
-- ESLint
-- Reactjs code snippets
-- Import Cost
+- **Prettier**: Code formatter for javascript
+- **Auto Import**
+- **Reactjs code snippets**
+- **Import Cost**: Calculates import cost in javascript files
+- **Markdown All in One**: Generally for my README.md file
+
+## References
+
+- [Get Started with form-data](https://www.form-data.com/?form=86cf0c10-a4a4-4dff-8bf2-c6518c1d9cfe)
+- [Form resources for static websites](https://gridsome.org/docs/guide-forms/)
+- [Submit a form with nodemailer and express](https://www.youtube.com/watch?v=EPnBO8HgyRU)
+- [Temporary email with Ethereal](https://ethereal.email/create)
+- [Get started with react router](https://reacttraining.com/react-router/web/example/basic)
+- [Install react-router-dom with npm](https://www.npmjs.com/package/react-router-dom)
--- src/Home.js
+++ src/Home.js
@@ -1,102 +1,118 @@
 //import liraries
-import React, { Component } from 'react';
-import { Container, Row, Col, Image, Jumbotron } from 'react-bootstrap';
-import { FaArrowDown, FaCheckCircle } from 'react-icons/fa';
-import { IconContext } from 'react-icons';
-import ContactForm from './ContactForm';
+import React, { Component } from "react";
+import { Container, Row, Col, Image, Jumbotron } from "react-bootstrap";
+import { FaArrowDown, FaCheckCircle } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import ContactForm from "./ContactForm";
 
 // create a component
 class Home extends Component {
-	//   constructor(props) {
-	//     super(props);
-	//   }
-	renderAboutMe() {
-		const MyPhoto = require('./picture.png');
-		return (
-			<div>
-				<h2>About Me</h2>
-				<Jumbotron className="jumbo-aboutme">
-					<Container>
-						<Row>
-							<Col xs={12} md={3}>
-								<Image src={MyPhoto} roundedCircle fluid />
-							</Col>
-							<Col xs={12} md={9}>
-								<h5>Hi, I am Rupal.</h5>
-								<p>{'I am a computer science student at Cal Poly presently.'}</p>
-								<p>
-									{`I like to work on interesting coding projects, teach, hike,
+  //   constructor(props) {
+  //     super(props);
+  //   }
+  renderAboutMe() {
+    const MyPhoto = require("./picture.png");
+    return (
+      <div>
+        <h2>About Me</h2>
+        <Jumbotron className="jumbo-aboutme">
+          <Container>
+            <Row>
+              <Col xs={12} md={3}>
+                <Image src={MyPhoto} roundedCircle fluid />
+              </Col>
+              <Col xs={12} md={9}>
+                <h5>Hi, I am Rupal.</h5>
+                <p>
+                  {"I am a computer science student at Cal Poly presently."}
+                </p>
+                <p>
+                  {`I like to work on interesting coding projects, teach, hike,
                   and read classic fiction. Besides computer sicence, I deeply
                   enjoy philosophy, and history.`}
-								</p>
-								<p>
-									{`Feel free to explore the projects I have worked on, the
+                </p>
+                <p>
+                  {`Feel free to explore the projects I have worked on, the
                   classes I have taken, and the places I have worked at over the
                   last two years on this website. If you have any suggestions
                   for me or exciting oppurtunities you believe I would be a good
-                  fit for, please do reach out (contact details`}{' '}
-									<FaArrowDown />
-									{`)!`}
-								</p>
-							</Col>
-						</Row>
-					</Container>
-				</Jumbotron>
-			</div>
-		);
-	}
+                  fit for, please do reach out (contact details`}{" "}
+                  <FaArrowDown />
+                  {`)!`}
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
 
-	async handleSubmit(e) {
-		e.preventDefault();
-	}
+  async handleSubmit(e) {
+    e.preventDefault();
+  }
 
-	renderFormSubmitted(name, message, email, rating) {
-		return (
-			<Jumbotron style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-				<h3>Form submitted successfully</h3>
-				<IconContext.Provider value={{ color: 'green', className: 'global-class-name' }}>
-					<div style={{ padding: 10 }}>
-						<FaCheckCircle size={100} />
-					</div>
-				</IconContext.Provider>
-				<hr />
-				<h6 style={{ textAlign: 'center' }}>
-					Thank you for reaching out. I will get back to you through the provided email with a custom response
-					in around 5 business days. Here is what was submitted:
-				</h6>
-				<br />
-				<p>
-					<b>Name: </b> {name}
-				</p>
-				<p>
-					<b>Message: </b> {message}
-				</p>
-				<p>
-					<b>Email: </b> {email}
-				</p>
-				<p>
-					<b>Rating: </b> {rating ? rating : 'Not any'}
-				</p>
-			</Jumbotron>
-		);
-	}
+  renderFormSubmitted(name, message, email, rating) {
+    return (
+      <Jumbotron
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <h3>Form submitted successfully</h3>
+        <IconContext.Provider
+          value={{ color: "green", className: "global-class-name" }}
+        >
+          <div style={{ padding: 10 }}>
+            <FaCheckCircle size={100} />
+          </div>
+        </IconContext.Provider>
+        <hr />
+        <h6 style={{ textAlign: "center" }}>
+          Thank you for reaching out. I will get back to you through the
+          provided email with a custom response in around 5 business days. Here
+          is what was submitted:
+        </h6>
+        <br />
+        <p>
+          <b>Name: </b> {name}
+        </p>
+        <p>
+          <b>Message: </b> {message}
+        </p>
+        <p>
+          <b>Email: </b> {email}
+        </p>
+        <p>
+          <b>Rating: </b> {rating ? rating : "Not any"}
+        </p>
+      </Jumbotron>
+    );
+  }
 
-	render() {
-		const queryString = require('query-string');
-		const parsed = queryString.parse(this.props.location.search);
-		return (
-			<div className="container">
-				{this.renderAboutMe()}
+  render() {
+    const queryString = require("query-string");
+    const parsed = queryString.parse(this.props.location.search);
+    return (
+      <div className="container">
+        {this.renderAboutMe()}
 
-				<h2>Contact Me</h2>
-				{parsed.submit ? (
-					this.renderFormSubmitted(parsed['?name'], parsed.message, parsed.email, parsed.rating)
-				) : (
-					<ContactForm />
-				)}
-			</div>
-		);
-	}
+        <h2>Contact Me</h2>
+        {parsed.submit ? (
+          this.renderFormSubmitted(
+            parsed["?name"],
+            parsed.message,
+            parsed.email,
+            parsed.rating
+          )
+        ) : (
+          <ContactForm />
+        )}
+      </div>
+    );
+  }
 }
 
 //make this component available to the app
