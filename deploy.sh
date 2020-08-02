23fa6b5fd3cf66d035d7ff394dd131777f33b698
Rupal Totale <rupaltotale@gmail.com>
2020-08-02 12:55:11 -0700

    Updates

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "===============> Running GetData.js"
-node GetData.js
-echo "===============> Publishing latest data to GitHub"
-git add src/Data/*
-git commit -m "Latest data being published"
-git push
-echo "===============> Deploying - its going live!"
-yarn run deploy
